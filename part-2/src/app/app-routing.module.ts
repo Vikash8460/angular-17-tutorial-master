f82087fff0fb432d19d3d5e16f75d00d11bc3002
Vikash8460 <Vikashkamat8080@gmail.com>
2024-04-12 12:30:36 +0530

    routing home.componet in app.routing.modules.ts

--- part-2/src/app/app-routing.module.ts
+++ part-2/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './account/register/register.component';
 import { LoginComponent } from './account/login/login.component';
+import { HomeComponent } from './home/home/home.component';
 
 const routes: Routes = [
-  // { path: '', component: HomeComponent },
+   { path: '', component: HomeComponent },
   { path: 'account/login', component: LoginComponent },
   { path: 'account/register', component: RegisterComponent },
 
