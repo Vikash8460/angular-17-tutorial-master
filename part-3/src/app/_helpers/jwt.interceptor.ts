062db1ca3efeea907fa567b3cac43f326380580a
Vikash8460 <Vikashkamat8080@gmail.com>
2024-04-12 16:41:11 +0530

    write the code inside the jwt.interceptor.ts.

--- part-3/src/app/_helpers/jwt.interceptor.ts
+++ part-3/src/app/_helpers/jwt.interceptor.ts
@@ -1,5 +1,24 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+import { AccountService } from '../_services/account.service';
+import { environment } from '../../environments/environment';
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+    constructor(private accountService: AccountService) { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // add auth header with jwt if user is logged in and request is to the api url
+        const user = this.accountService.userValue;
+        const isLoggedIn = user?.token;
+        const isApiUrl = request.url.startsWith(environment.apiUrl);
+        if (isLoggedIn && isApiUrl) {
+            request = request.clone({
+                setHeaders: { Authorization: `Bearer ${user.token}` }
+            });
+        }
+
+        return next.handle(request);
+    }
+}
