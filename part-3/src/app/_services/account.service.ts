1f0d96a1f32ae937a117c4f411163e2663e67885
Vikash8460 <Vikashkamat8080@gmail.com>
2024-04-12 16:37:45 +0530

    write the code inside the account.service.ts

--- part-3/src/app/_services/account.service.ts
+++ part-3/src/app/_services/account.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { environment } from '../../environments/environment';
+import { User } from '../_models/user';
+@Injectable({ providedIn: 'root' })
 export class AccountService {
+    private userSubject: BehaviorSubject<User | null>;
+    public user: Observable<User | null>;
 
-  constructor() { }
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) {
+        this.userSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('user')!));
+        this.user = this.userSubject.asObservable();
+    }
+
+    public get userValue() {
+        return this.userSubject.value;
+    }
+
+    login(username: string, password: string) {
+        return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { username, password })
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+                this.userSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('user');
+        this.userSubject.next(null);
+        this.router.navigate(['/account/login']);
+    }
 }
