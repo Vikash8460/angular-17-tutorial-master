56b868b39bf35607b980dbc4b17dfd9dbedc89c1
Vikash8460 <Vikashkamat8080@gmail.com>
2024-04-12 12:27:55 +0530

    routing in app.routing.modules.ts

--- part-2/src/app/app-routing.module.ts
+++ part-2/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './account/register/register.component';
+import { LoginComponent } from './account/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  { path: 'account/login', component: LoginComponent },
+  { path: 'account/register', component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
