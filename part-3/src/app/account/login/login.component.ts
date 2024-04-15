328cd24e7a096307884582084a11452de9b71acf
Vikash8460 <Vikashkamat8080@gmail.com>
2024-04-15 15:06:44 +0530

    write the code login.component.ts

--- part-3/src/app/account/login/login.component.ts
+++ part-3/src/app/account/login/login.component.ts
@@ -1,34 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AccountService } from '../../_services/account.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit {
-  
-  form!: FormGroup;
-  loading = false;
-  submitted = false;
-  error?: string;
-
-  constructor(  
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private accountService: AccountService
-  ) {
-    // redirect to home if already logged in
-    if (this.accountService.userValue) {
-        this.router.navigate(['/']);
-    }
-}
-
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+export class LoginComponent {
 
 }
